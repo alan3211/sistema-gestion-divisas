b5896f80678a3ca3f6848814e89ba0e199160177
Alan3211 <aalanhernandezs@gmail.com>
2023-10-23 23:59:01 -0600

    Se corrigen bugs

--- src/utils/constantes.js
+++ src/utils/constantes.js
@@ -1,5 +1,5 @@
-const URL = 'https://grocerys-back--9nkih98.wittysmoke-209c31ac.eastus.azurecontainerapps.io/';
-//const URL = 'http://192.168.101.45:9000/';
+//const URL = 'https://grocerys-back--9nkih98.wittysmoke-209c31ac.eastus.azurecontainerapps.io/';
+const URL = 'http://192.168.101.45:9000/';
 //const URL = 'http://localhost:9000/';
 
 export const LOGIN_URL = `${URL}login/`;
